20db8b4167bd187e25298d4c264f0f4e8c575b28
KNSTNMarkevich <konstantinmarkevich@gmail.com>
2020-10-24 00:18:53 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {};
+bracketsConfig.forEach((elem) => { 
+  brackets[elem[1]] = elem[0];
+  });
+  
+  let newArr = [];
+  
+for (let i = 0; i < str.length; i++) {
+  if (i === 0) {
+          newArr.push(str[0]);
+      } else {
+    if (newArr.length !== 0 && newArr[newArr.length - 1] === brackets[str[i]]) {
+      newArr.pop();
+    } else {
+      newArr.push(str[i]);
+    }	
+  }	
+}
+return newArr.length === 0;
 }
